fcb2f64bd5696f2fb645321a42a414e9bfb0b7ae
jamesCodes808 <jamesian.r.solima@gmail.com>
2023-01-13 13:40:19 -0800

    problem2

--- js/app.js
+++ js/app.js
@@ -50,7 +50,7 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
-
+    
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
